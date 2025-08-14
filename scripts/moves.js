fa1168624cef687895271f36ddd7e816be46693c
David Majholt <d.boy.dragoz@gmail.com>
2025-08-14 07:29:40 +0200

    Added simple JS example.

--- scripts/moves.js
+++ scripts/moves.js
@@ -1,10 +1,18 @@
 const button = document.querySelector(".extra1");
 const mainLink = document.querySelector("link:first-of-type");
 const styles = mainLink.dataset.css.split(",");
-let currentIndex = 1;
+let currentIndex = 0;
 
 function changeStylesheet() {
-	(currentIndex <= styles.length - 1) ? currentIndex++ : currentIndex = 1;
+	(currentIndex <= styles.length - 1) ? currentIndex++ : currentIndex = 0;
+
+	// if (currentIndex <= styles.length -1) {
+	// 	currentIndex++
+	// } else {
+	// 	currentIndex = 0;
+	// }
+
 	mainLink.href = styles[currentIndex - 1];
 }
+
 button.addEventListener("click", changeStylesheet); changeStylesheet();
