5a620cf763f649e4cb40908edce9c372c1abe857
David Majholt <d.boy.dragoz@gmail.com>
2025-08-14 07:34:51 +0200

    Fixed Bugs

--- scripts/moves.js
+++ scripts/moves.js
@@ -4,7 +4,7 @@ const styles = mainLink.dataset.css.split(",");
 let currentIndex = 0;
 
 function changeStylesheet() {
-	(currentIndex <= styles.length - 1) ? currentIndex++ : currentIndex = 0;
+	(currentIndex <= styles.length - 1) ? currentIndex++ : currentIndex = 1;
 
 	// if (currentIndex <= styles.length -1) {
 	// 	currentIndex++
