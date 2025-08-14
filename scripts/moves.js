6093c1ed2263584035b8032b7ae8e928d937648f
David Majholt <d.boy.dragoz@gmail.com>
2025-08-14 07:18:58 +0200

    Added Movement Between Pages & Started working on Next Page

---
+++ scripts/moves.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".extra1");
+const mainLink = document.querySelector("link:first-of-type");
+const styles = mainLink.dataset.css.split(",");
+let currentIndex = 1;
+
+function changeStylesheet() {
+	(currentIndex <= styles.length - 1) ? currentIndex++ : currentIndex = 1;
+	mainLink.href = styles[currentIndex - 1];
+}
+button.addEventListener("click", changeStylesheet); changeStylesheet();
